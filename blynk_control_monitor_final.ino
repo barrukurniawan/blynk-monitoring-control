e75789bcc694652465936d273ac3538af181f71d
barru kurniawan <32356232+barrukurniawan@users.noreply.github.com>
2020-01-20 16:45:32 +0700

    Create blynk_control_monitor_final.ino

---
+++ blynk_control_monitor_final.ino
@@ -0,0 +1,83 @@
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#define trigPin  D8
+#define echoPin  D7
+#define ledrendah D2
+#define ledtinggi D3
+
+char ssid[] = "gigel.id";
+char pass[] = "cisanggiri14";
+char auth[] = "N5ODWRJ5xcgDVGM3UQs0xGRDVxc7_fb9";
+
+long duration;
+int distance;
+
+BlynkTimer timer;
+
+BLYNK_WRITE(V1)
+{
+  int nilai = random(1,999);
+  
+  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  if (pinValue == 1) {
+    Blynk.tweet("Bendungan dibuka! \n #IoT " + String(nilai));
+    Serial.println("send tweet! " + String(nilai));    
+  } else {
+    Serial.println("Off tweet");
+  }
+
+  // process received value
+}
+
+void sendSensor()
+{
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance= duration*0.034/2;
+  
+  Serial.println(distance);
+  if (distance <= 80){
+    digitalWrite(ledrendah, HIGH);
+    digitalWrite(ledtinggi, LOW);
+  } else if (distance >= 80) {
+    digitalWrite(ledtinggi, HIGH);
+    digitalWrite(ledrendah, LOW);
+  } else if (distance >= 3000){
+    Blynk.tweet("Waw ketinggian air capai lebih dari 2000cm :)\n #arduino #IoT #blynk @blynk_app");
+  }
+  Blynk.virtualWrite(V5, distance);
+  
+}
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  pinMode(ledrendah, OUTPUT);
+  pinMode(ledtinggi, OUTPUT);
+  
+  // Debug console
+  Serial.begin(115200);
+
+  Blynk.begin(auth, ssid, pass);
+  
+//  Blynk.tweet("Hey, Sobat UNAS!! \nWe are Bendungan UNAS!");
+
+  timer.setInterval(2000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
